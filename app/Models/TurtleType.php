f6bc9e7e7c70152c5ee550f8992fc6c474d775da
Kenji Hikmatullah <iniemailkenji@gmail.com>
2020-11-28 16:00:55 +0700

    fix bug in turtle type model

--- app/Models/TurtleType.php
+++ app/Models/TurtleType.php
@@ -10,5 +10,5 @@ class TurtleType extends Model
 {
     use HasFactory, SoftDeletes;
 
-    protected $fillable = ['image_path, name, description, created_at, updated_at'];
+    protected $fillable = ['image_path', 'name', 'description', 'created_at', 'updated_at'];
 }
